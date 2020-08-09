3b8438a18e37704fb1c9afbe20ef9fcbb85abde0
unknown <rkurbetti30@gmail.com>
2020-08-10 01:03:24 +0530

    dasd

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const cors = require('cors');
-const userRouter = require('./Backend/Routes/users');
-const excerciseRouter = require('./Backend/Routes/excercises');
+const userRouter = require('./Routes/users');
+const excerciseRouter = require('./Routes/excercises');
 const path = require("path");
 require('dotenv').config();
 const mongoose = require('mongoose');
@@ -10,23 +10,24 @@ const port = process.env.PORT||3200;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
-app.use('/users',userRouter);
-app.use('/excercises',excerciseRouter);
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('build'));
-}
-app.get("*", (req, res) => {
- res.sendFile(path.join(__dirname,'./build/index.html'));
-});
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology: true});
 const connection = mongoose.connection;
 
 connection.once('open', ()=>{
 	console.log('Mongodb db connection established succesfully');
 });
+
+app.use('/users',userRouter);
+app.use('/excercises',excerciseRouter);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname,'build','index.html'));
+	});
+}
 
 app.listen(port,()=>{
 	console.log(`Server running on port: ${port}`);
