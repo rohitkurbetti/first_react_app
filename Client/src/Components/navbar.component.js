13cf710b9231517c89ae3b737622c447c3d30ff8
unknown <rkurbetti30@gmail.com>
2020-08-10 12:58:44 +0530

    NewChanges_10Aug20_12:58

--- Client/src/Components/navbar.component.js
+++ Client/src/Components/navbar.component.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import "../../node_modules/bootstrap/dist/js/bootstrap.min.js"
+import "../../node_modules/jquery/dist/jquery.min.js";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
                 <Link to="/" className="navbar-brand">ExcerciseTracker</Link>
-                <div className="collapse navbar-collapse">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        <li className="navbar-item">
+                        <li className="navbar-item active">
                             <Link to="/" className="nav-link">Excercises</Link>
                         </li>
 
